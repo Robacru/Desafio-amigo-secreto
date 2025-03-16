0191adfc176ebb820f16f0e96c94bff90649b506
Robacru <rochbacru@gmail.com>
2025-03-16 16:18:30 -0300

    Proyecto terminado
    Proyecto amigo secreto terminado de acuerdo a los requerimientos del curso

---
+++ README.md
@@ -0,0 +1,37 @@
+# Desafío: Lista de Amigos y Sorteo
+
+El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Este proyecto permite a los usuarios gestionar una lista de amigos y realizar un sorteo de amigo secreto.
+
+**Interfaz Principal**
+![Pantalla Inicial](assets/Pantalla-de-inicio.png)
+
+## Características
+- Agregar amigos a una lista, validando que no se ingresen nombres duplicados.
+- Visualizar la lista de amigos actualizada en la interfaz.
+- Realizar el sorteo de un amigo secreto de manera aleatoria.
+- Reiniciar la lista de amigos después del sorteo.
+
+## Cómo Usar el Proyecto
+1. **Agregar amigos:**
+   - Escribe un nombre en el campo de texto y presiona el botón "Agregar".
+   - Si el nombre está vacío o ya existe en la lista, se mostrará una alerta.
+2. **Actualizar la lista de amigos:**
+   - Cada vez que agregues un amigo, la lista se actualizará automáticamente.
+
+   **Lista de amigos**
+   ![Lista de amigos ingresados](assets/Amigos-ingresados.png)
+
+3. **Sortear un amigo secreto:**
+   - Presiona el botón "Sortear" para seleccionar un amigo aleatorio de la lista.
+   - El nombre del amigo sorteado se mostrará en la interfaz.
+   - La lista de amigos se limpiará automáticamente.
+
+   **Amigo sorteado**
+   ![Amigo sorteado](assets/Amigo-sorteado.png)
+
+
+## Ejecución del Proyecto
+
+1. Clona este repositorio en tu computadora:
+   ```bash
+   git clone https://github.com/Robacru/Desafio-amigo-secreto.git
--- app.js
+++ app.js
@@ -1,1 +1,72 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+// función para agregar un amigo a la lista
+function agregarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nombreAmigo = inputAmigo.value;
+
+    // validar que se ingrese un nombre
+    if (nombreAmigo === "") {
+        alert('Debes ingresar un nombre');
+        return;
+    }    
+
+    // validar que el nombre no esté repetido
+    if (amigos.includes(nombreAmigo)) {
+        alert(`El nombre ${nombreAmigo} ya fue ingresado, no se permiten nombres duplicados`);
+        return;
+    }
+
+    // agregar el nombre a la lista
+    amigos.push(nombreAmigo);
+
+    // limpia el nombre ya ingresado
+    inputAmigo.value = "";
+
+    // volver el cursor a la casilla de nombre
+    inputAmigo.focus();
+
+    // actualiza la lista de amigos
+    actualizarListaAmigos();
+
+}
+// función para actualizar la lista de amigos
+function actualizarListaAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ""; // limpia la lista previa
+
+    amigos.forEach((amigo, index) => {
+        const li = document.createElement('li');
+        li.textContent = `${index + 1}. ${amigo}`;
+        lista.appendChild(li);
+    });
+}
+
+// función para seleccionar un amigo aleatorio
+function sortearAmigo() {
+// limpiar el resultado anterior
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = "";
+
+    if(amigos.length === 0){
+        alert('No hay amigos en la lista para sortear');
+        return;
+    }
+        
+    // generar un número aleatorio
+    const index = Math.floor(Math.random() * amigos.length);
+
+    // obtener el amigo seleccionado o sorteado
+    const amigoSorteado = amigos[index].toUpperCase(); // utiliza el índice aleatorio para el amigo
+
+    // mostrar el amigo sorteado en el HTML
+    resultado.innerHTML = `El amigo secreto sorteado es: ${amigoSorteado}`;
+
+    // limpiar la lista de amigos
+    amigos = [];
+    actualizarListaAmigos();
+
+}
